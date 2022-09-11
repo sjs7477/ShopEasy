1b9d53ee3c28725fcdc2de9b0fe8a28af474fd9c
Shreyas Sambasivam <shreyas@Shreyass-MacBook-Air.local>
2022-09-11 18:29:06 -0400

    Automation testing for successful test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.assertEquals;
 
 public class HelloWorldTest {
     @Test
-//    public void test(){
-//        assertEquals("Testing Hello world string","Hello World",helloWorld.welcome());
-//    }
-
     public void test(){
-        assertEquals("Testing Hello world string","Hello Word",helloWorld.welcome());
+        assertEquals("Testing Hello world string","Hello World",helloWorld.welcome());
     }
+
+//    public void test(){
+//        assertEquals("Testing Hello world string","Hello Word",helloWorld.welcome());
+//    }
 
 }

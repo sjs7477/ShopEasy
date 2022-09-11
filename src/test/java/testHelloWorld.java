d21ef75735eae641228e2f7607b3b7dc3aae4ac7
Shreyas Sambasivam <shreyas@Shreyass-MacBook-Air.local>
2022-09-11 16:41:03 -0400

    Check for fail test case automation

--- src/test/java/testHelloWorld.java
+++ src/test/java/testHelloWorld.java
@@ -5,7 +5,12 @@ import static org.junit.Assert.assertEquals;
 
 public class testHelloWorld {
     @Test
+//    public void test(){
+//        assertEquals("Testing Hello world string","Hello World",helloWorld.welcome());
+//    }
+
     public void test(){
-        assertEquals("Testing Hello world string","Hello World",helloWorld.welcome());
+        assertEquals("Testing Hello world string","Hello Word",helloWorld.welcome());
     }
+
 }
